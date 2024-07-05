ae62bf74705e3e2c567bcdedfcb3042baccea858
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-05 19:29:24 +0000

    updated logo

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -27,7 +27,9 @@ if (!isset($_SESSION['auth'])) {
     <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
   <div class="container">
     <a class="navbar-brand" href="/">
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAXVBMVEVHcEzi7vzk7/vt8/n39vZniNK11v/H4//Z6vzv8/f29vWy1f+axf+Vwf+Luf6Kufx6rPtOjfZKivVhmvh6rPpQjvVEhvSNvP5DhO88eeotZd0rZNEnYdspX9EnXNGKIiMYAAAAH3RSTlMALnSgsBmj//////////+e/////3Gu/66d/v80/5OvsiUU+QAAASxJREFUeAGVjtGSgyAMALEWbSVQiT2uqvT/P/MScqBWX7p5YWZ3QtRXVJf6eq0v1bnVTZu43buu1kdftzeyknSUfC5vDQAY0wp3SqqdNxYIa8GcFhUU7H/R7YpV85K1yL6BXWE+L9UknHPZSyFnaMU8oPeI6PuSlENlBQzoGfRk5YxSsK968gK69ROhouDpV1C8dTm48Akbjx6sNK3QUPDjN+AAVjDHAJnQ5wJMa+CXgpf3YRzHlNAEm3E0j3TkODHBY8LbDU8KZpwSy0nhZkXIAiKwDjQO5APrFROnDKI0YeAEnH8pRk8FtgIOAz21SsR9IREjC4hphU4ojCqjF3ZLmhGL16owi6RZKAnIeprVBp3twvC6Ras9UWwmqgM6brRWp8wxvt8xzuob/gCnVSzVcGJ2AwAAAABJRU5ErkJggg==" alt="" width="30" height="30">
+      <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-globe-americas" viewBox="0 0 16 16">
+        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484q-.121.12-.242.234c-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
+      </svg>
       COSC4806
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
