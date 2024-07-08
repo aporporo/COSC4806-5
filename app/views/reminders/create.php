415ff6882c3c079fa38b8a60bc4e31481be145fe
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-08 17:42:29 +0000

    style formatting to create and update, also included breadcrumbs

--- app/views/reminders/create.php
+++ app/views/reminders/create.php
@@ -6,12 +6,35 @@
         background-repeat: no-repeat;
         background-attachment: fixed;
     }
+    .breadcrumb .breadcrumb-item a {
+        color: white; 
+        text-decoration: none; 
+      }
+
+      .breadcrumb .breadcrumb-item::before {
+        color: white;
+      }
+
+      .breadcrumb .breadcrumb-item.active {
+        color: #bbb; 
+      }
+
+      .breadcrumb .breadcrumb-item a:hover {
+        color: #ddd; 
+      } 
 </style>
 <main role="main" class="container">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Create a reminder</h1>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/reminders">Reminders</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">Create</li>
+                  </ol>
+                </nav>
+                <h1 style="color: white">Create a reminder</h1>
                 
             </div>
         </div>
@@ -21,7 +44,7 @@
     <div class="vh-50">
         <div class="container py-2 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-8">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div style="border-radius: 1rem; background-color: #6c757d;">
                         <div class="card-body p-5 text-center">
                             
--- app/views/reminders/update.php
+++ app/views/reminders/update.php
@@ -6,12 +6,35 @@
         background-repeat: no-repeat;
         background-attachment: fixed;
     }
+    .breadcrumb .breadcrumb-item a {
+        color: white; 
+        text-decoration: none; 
+      }
+
+      .breadcrumb .breadcrumb-item::before {
+        color: white;
+      }
+
+      .breadcrumb .breadcrumb-item.active {
+        color: #bbb; 
+      }
+
+      .breadcrumb .breadcrumb-item a:hover {
+        color: #ddd; 
+      } 
 </style>
 <main role="main" class="container">
     <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Update a reminder</h1>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item"><a href="/reminders">Reminders</a></li>
+                      <li class="breadcrumb-item active" aria-current="page">Update</li>
+                  </ol>
+                </nav>
+                <h1 style="color: white">Update a reminder</h1>
                 
             </div>
         </div>
@@ -21,7 +44,7 @@
     <div class="vh-50">
         <div class="container py-2 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-8">
+                <div  class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div style="border-radius: 1rem; background-color: #6c757d;">
                         <div class="card-body p-5 text-center">
                             
@@ -31,19 +54,19 @@
                             <fieldset>
                               <div data-mdb-input-init class="form-outline mb-3">
                                   <div class="form-group row">
-                                  <label for="reminder_id" class="col-sm-2 col-form-label text-white">Reminder ID:</label>
-                                      <div class="col-sm-10">
+                                  <label for="reminder_id" class="col-sm-3 col-form-label text-white">Reminder ID:</label>
+                                      <div class="col-sm-9">
                                   <input required type="text" readonly disabled class="form-control form-control-lg" name="reminder_id" value="<?php echo $data['reminder']['id'];?> ">
                                       </div>
                                   </div>
                                   <br>
                                   <div class="form-group row">
-                                  <label for="subject" class="col-sm-2 col col-form-label text-white ">Subject:</label>
-                                <div class="col-sm-10">
+                                  <label for="subject" class="col-sm-3 col col-form-label text-white ">Subject:</label>
+                                <div class="col-sm-9">
                                       <input required type="text" class="form-control form-control-lg" name="subject" value="<?php echo $data['reminder']['subject'];?>">
                                 </div>
                               </div>
-                              
+                              <br>
 
                                 <button type="submit" class="btn btn-primary btn-lg">Update Reminder</button>
                             </fieldset>
